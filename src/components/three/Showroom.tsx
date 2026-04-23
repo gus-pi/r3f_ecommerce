ebea09a03e5a4fbeddd93f05f965b8f3a58226df
PieMenard <pinedo.m.gustavo@gmail.com>
2026-04-23 17:00:43 -0400

    fix model, add raycasting

--- src/components/three/Showroom.tsx
+++ src/components/three/Showroom.tsx
@@ -1,10 +1,28 @@
 import { useGLTF } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
+import * as THREE from 'three';
 
 const Showroom = () => {
     const gltf = useGLTF('/models/custom.glb');
+    const { raycaster } = useThree();
+
+    const shoesClick = () => {
+        const intersects = raycaster.intersectObjects(gltf.scene.children, true);
+
+        if (intersects.length > 0) {
+            const firstObj = intersects[0].object as THREE.Mesh;
+            const firstMat = firstObj.material as THREE.MeshStandardMaterial;
+            const cloneMat = firstMat.clone();
+
+            firstObj.material = cloneMat;
+            const mat = firstObj.material as THREE.MeshStandardMaterial;
+
+            mat.color = new THREE.Color('red');
+        }
+    };
     return (
         <>
-            <primitive object={gltf.scene} />
+            <primitive object={gltf.scene} onClick={shoesClick} />
 
             {/* <mesh
                 rotation={[
